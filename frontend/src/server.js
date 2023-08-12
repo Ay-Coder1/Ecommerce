5b6f4b1ba28bf46f73bb36f40e800bedc0d47cd5
Ay-coder <ayomidebabarinde07@gmail.com>
2023-08-12 13:04:21 +0100

    update4

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://ecommerce-api-delta.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://ecommerce-api-delta.vercel.app:8000/";
