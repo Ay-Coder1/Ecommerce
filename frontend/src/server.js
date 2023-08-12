a99902a4d3d0543807928437908183f4407ae746
Ay-coder <ayomidebabarinde07@gmail.com>
2023-08-12 13:57:04 +0100

    update5

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://ecommerce-api-delta.vercel.app/api/v2";
+export const server = "https://ecommerce-api-one-olive.vercel.app/api/v2";
 
-export const backend_url = "https://ecommerce-api-delta.vercel.app/";
+export const backend_url = "https://ecommerce-api-one-olive.vercel.app/";
